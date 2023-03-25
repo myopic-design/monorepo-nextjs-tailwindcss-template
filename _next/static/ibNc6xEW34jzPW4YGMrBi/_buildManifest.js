04b30f35d52848c740fee89789b0b2cc639f3ca3
unindented <unindented@users.noreply.github.com>
2023-03-25 06:50:06 +0000

    deploy: f49079fdb8c790eb0a3ce76f154f2ca9bd98aaf0

--- _next/static/ibNc6xEW34jzPW4YGMrBi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d39796b874cacb18.js"],"/_error":["static/chunks/pages/_error-c6030142f67dffe2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
