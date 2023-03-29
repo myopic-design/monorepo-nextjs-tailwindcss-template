87c21b22717299a900d08f1c2009512b2b627c49
unindented <unindented@users.noreply.github.com>
2023-03-29 07:00:19 +0000

    deploy: f652efa9c40afdbf50755ffa6b563045ed835237

--- _next/static/B1hPLWAg9l8Km7KU4ECRc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0f82a777d249d6e4.js"],"/404":["static/chunks/pages/404-1e86b566c1fb49df.js"],"/_error":["static/chunks/pages/_error-c6030142f67dffe2.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
